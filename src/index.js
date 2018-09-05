89c510f4d2fb79f3a6f5034e6677d0b67bbf25ec
Michael Boreham <bor3ham@gmail.com>
2018-09-05 14:23:47 +1000

    Add demo to repo

---
+++ src/index.js
@@ -0,0 +1,432 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Mireco from 'mireco'
+import moment from 'moment'
+import casual from 'casual-browserify'
+import beautify from 'json-beautify'
+import Cookies from 'js-cookie'
+
+const defaultValue = {
+  text: 'hi there',
+  checked: false,
+  time: null,
+  textarea: 'Wow this is two lines...\nIn this text box',
+  date: null,
+  duration: null,
+  datetime: +moment().utc(),
+  datetime_range: {start: null, end: null},
+}
+function randomValue() {
+  return {
+    text: casual.title,
+    checked: casual.coin_flip,
+    time: casual.coin_flip ? null : casual.integer(0, 24 * 60) * 60 * 1000,
+    textarea: casual.description,
+    date: casual.coin_flip ? null : moment.utc().startOf('day') - (casual.integer(-30, 30) * +moment.duration({days: 1})),
+    duration: casual.coin_flip ? null : casual.integer(0, 400) * +moment.duration({minutes: 30}),
+    datetime: casual.coin_flip ? null : +moment().utc() + casual.integer(
+      -1 * +moment.duration({days: 10}),
+      +moment.duration({days: 10}),
+    ),
+    datetime_range: {
+      start: casual.coin_flip ? null : +moment().utc().startOf('hour') + casual.integer(
+        -3 * 24,
+        3 * 24,
+      ) * +moment.duration({hours: 1}),
+      end: casual.coin_flip ? null : +moment().utc().startOf('hour') + casual.integer(
+        4 * 24,
+        7 * 24,
+      ) * +moment.duration({hours: 1}),
+    },
+  }
+}
+
+class Demo extends React.Component {
+  state = {
+    formValue: {
+      ...defaultValue,
+    },
+    flags: {
+      disabled: !!Cookies.get('disabled'),
+      intervalDisable: !!Cookies.get('intervalDisable'),
+      intervalRandomise: !!Cookies.get('intervalRandomise'),
+      intervalRemount: !!Cookies.get('intervalRemount'),
+      disableCss: !!Cookies.get('disableCss'),
+      blockMode: !!Cookies.get('blockMode'),
+
+      showText: !!Cookies.get('showText'),
+      showCheckbox: !!Cookies.get('showCheckbox'),
+      showTime: !!Cookies.get('showTime'),
+      showTextarea: !!Cookies.get('showTextarea'),
+      showDate: !!Cookies.get('showDate'),
+      showDuration: !!Cookies.get('showDuration'),
+      showDatetime: !!Cookies.get('showDatetime'),
+      showDatetimeRange: !!Cookies.get('showDatetimeRange'),
+    },
+    mountIndex: 0,
+  }
+  componentDidMount() {
+    this.interval = window.setInterval(this.onInterval, 3000)
+  }
+  componentWillUnmount() {
+    window.clearInterval(this.interval)
+  }
+  onInterval = () => {
+    if (this.state.flags.intervalDisable) {
+      this.toggleFlag('disabled')
+    }
+    if (this.state.flags.intervalRandomise) {
+      this.randomise()
+    }
+    if (this.state.flags.intervalRemount) {
+      this.setState(prevState => {
+        return {
+          mountIndex: prevState.mountIndex + 1,
+        }
+      })
+    }
+  }
+  setFlag = (key, value) => {
+    this.setState(prevState => {
+      let updates = {
+        flags: {
+          ...prevState.flags,
+        },
+      }
+      updates.flags[key] = value
+      return updates
+    }, () => {
+      if (this.state.flags[key]) {
+        Cookies.set(key, true)
+      }
+      else {
+        Cookies.remove(key)
+      }
+    })
+  }
+  toggleFlag = (key) => {
+    this.setFlag(key, !this.state.flags[key])
+  }
+  reset = () => {
+    this.setState({formValue: {
+      ...defaultValue,
+    }})
+  }
+  randomise = () => {
+    this.setState({formValue: randomValue()})
+  }
+  remount = () => {
+    this.setState(prevState => {
+      let updates ={
+        mountIndex: prevState.mountIndex + 1,
+      }
+      return updates
+    })
+  }
+  updateFormValue = (key, value) => {
+    this.setState(prevState => {
+      let updates = {
+        formValue: {
+          ...prevState.formValue,
+        },
+      }
+      updates.formValue[key] = value
+      return updates
+    })
+  }
+  handleSubmit = (event) => {
+    if (event) {
+      event.preventDefault()
+    }
+    alert('submitting value!')
+  }
+  render() {
+    let inlineSpace
+    if (!this.state.flags.blockMode) {
+      inlineSpace = (<span>{' '}</span>)
+    }
+    return (
+      <div>
+        <style>{`
+          body {
+            padding: 0;
+            margin: 0;
+          }
+          div.flag-column {
+            flex: 1;
+            min-width: 10rem;
+          }
+        `}</style>
+        <div style={{
+          background: '#464656',
+          color: '#fff',
+          padding: '3rem',
+        }}>
+          <h1>Mireco demo form</h1>
+          <div style={{display: 'flex', flexWrap: 'wrap'}}>
+            <div className="flag-column">
+              <h2>Form settings</h2>
+              <Mireco.block.Checkbox
+                value={this.state.flags.disableCss}
+                onChange={(newValue) => {
+                  this.setFlag('disableCss', newValue)
+                }}
+                label="Disable Mireco CSS"
+              />
+              <Mireco.block.Checkbox
+                value={this.state.flags.blockMode}
+                onChange={(newValue) => {
+                  this.setFlag('blockMode', newValue)
+                }}
+                label="Block mode"
+              />
+              <Mireco.block.Checkbox
+                value={this.state.flags.disabled}
+                onChange={(newValue) => {
+                  this.setFlag('disabled', newValue)
+                }}
+                label="Disabled"
+              />
+            </div>
+            <div className="flag-column">
+              <h2>Interval changes</h2>
+              <Mireco.block.Checkbox
+                value={this.state.flags.intervalDisable}
+                onChange={(newValue) => {
+                  this.setFlag('intervalDisable', newValue)
+                }}
+                label="Periodically disable"
+              />
+              <Mireco.block.Checkbox
+                value={this.state.flags.intervalRandomise}
+                onChange={(newValue) => {
+                  this.setFlag('intervalRandomise', newValue)
+                }}
+                label="Periodically randomise"
+              />
+              <Mireco.block.Checkbox
+                value={this.state.flags.intervalRemount}
+                onChange={(newValue) => {
+                  this.setFlag('intervalRemount', newValue)
+                }}
+                label="Periodically remount"
+              />
+            </div>
+            <div className="flag-column">
+              <h2>Basic inputs</h2>
+              <Mireco.block.Checkbox
+                value={this.state.flags.showText}
+                onChange={(newValue) => {
+                  this.setFlag('showText', newValue)
+                }}
+                label="Show text input"
+              />
+              <Mireco.block.Checkbox
+                value={this.state.flags.showCheckbox}
+                onChange={(newValue) => {
+                  this.setFlag('showCheckbox', newValue)
+                }}
+                label="Show checkbox input"
+              />
+              <Mireco.block.Checkbox
+                value={this.state.flags.showTime}
+                onChange={(newValue) => {
+                  this.setFlag('showTime', newValue)
+                }}
+                label="Show time input"
+              />
+              <Mireco.block.Checkbox
+                value={this.state.flags.showTextarea}
+                onChange={(newValue) => {
+                  this.setFlag('showTextarea', newValue)
+                }}
+                label="Show textarea input"
+              />
+              <Mireco.block.Checkbox
+                value={this.state.flags.showDate}
+                onChange={(newValue) => {
+                  this.setFlag('showDate', newValue)
+                }}
+                label="Show date input"
+              />
+              <Mireco.block.Checkbox
+                value={this.state.flags.showDuration}
+                onChange={(newValue) => {
+                  this.setFlag('showDuration', newValue)
+                }}
+                label="Show duration input"
+              />
+            </div>
+            <div className="flag-column">
+              <h2>Compound inputs</h2>
+              <Mireco.block.Checkbox
+                value={this.state.flags.showDatetime}
+                onChange={(newValue) => {
+                  this.setFlag('showDatetime', newValue)
+                }}
+                label="Show datetime input"
+              />
+              <Mireco.block.Checkbox
+                value={this.state.flags.showDatetimeRange}
+                onChange={(newValue) => {
+                  this.setFlag('showDatetimeRange', newValue)
+                }}
+                label="Show datetime range input"
+              />
+            </div>
+          </div>
+          <div style={{marginBottom: '1rem'}}>
+            <Mireco.Button
+              onClick={this.randomise}
+              className="secondary"
+            >
+              Randomise
+            </Mireco.Button>
+            {' '}
+            <Mireco.Button
+              onClick={this.remount}
+              className="secondary"
+            >
+              Remount
+            </Mireco.Button>
+            {' '}
+            <Mireco.Button
+              onClick={this.reset}
+              className="secondary"
+            >
+              Reset
+            </Mireco.Button>
+          </div>
+        </div>
+        <div style={{
+          background: '#f5f5f5',
+          padding: '3rem',
+          margin: '0',
+        }}>
+          <h2>Form state value</h2>
+          <pre style={{
+            whiteSpace: 'pre-wrap',
+            fontFamily: 'monospace',
+          }}>
+            {beautify(this.state.formValue, null, 2, 80)}
+          </pre>
+        </div>
+        <form
+          onSubmit={this.handleSubmit}
+          key={`form-mount-${this.state.mountIndex}`}
+          style={{margin: '10rem 1rem'}}
+        >
+          {this.state.flags.showText && (
+            <Mireco.Text
+              value={this.state.formValue.text}
+              onChange={(newValue) => {
+                this.updateFormValue('text', newValue)
+              }}
+              placeholder="Single text"
+              disabled={this.state.flags.disabled}
+              block={this.state.flags.blockMode}
+            />
+          )}
+          {this.state.flags.showText && inlineSpace}
+          {this.state.flags.showCheckbox && (
+            <Mireco.Checkbox
+              value={this.state.formValue.checked}
+              onChange={(newValue) => {
+                this.updateFormValue('checked', newValue)
+              }}
+              label="Checked or not?"
+              disabled={this.state.flags.disabled}
+              block={this.state.flags.blockMode}
+            />
+          )}
+          {this.state.flags.showCheckbox && inlineSpace}
+          {this.state.flags.showTime && (
+            <Mireco.Time
+              value={this.state.formValue.time}
+              onChange={(newValue) => {
+                this.updateFormValue('time', newValue)
+              }}
+              disabled={this.state.flags.disabled}
+              block={this.state.flags.blockMode}
+            />
+          )}
+          {this.state.flags.showTime && inlineSpace}
+          {this.state.flags.showTextarea && (
+            <Mireco.Textarea
+              value={this.state.formValue.textarea}
+              onChange={(newValue) => {
+                this.updateFormValue('textarea', newValue)
+              }}
+              placeholder="Multiline text..."
+              disabled={this.state.flags.disabled}
+              block={this.state.flags.blockMode}
+            />
+          )}
+          {this.state.flags.showTextarea && inlineSpace}
+          {this.state.flags.showDate && (
+            <Mireco.Date
+              value={this.state.formValue.date}
+              onChange={(newValue) => {
+                this.updateFormValue('date', newValue)
+              }}
+              disabled={this.state.flags.disabled}
+              block={this.state.flags.blockMode}
+            />
+          )}
+          {this.state.flags.showDate && inlineSpace}
+          {this.state.flags.showDuration && (
+            <Mireco.Duration
+              value={this.state.formValue.duration}
+              onChange={(newValue) => {
+                this.updateFormValue('duration', newValue)
+              }}
+              disabled={this.state.flags.disabled}
+              block={this.state.flags.blockMode}
+            />
+          )}
+          {this.state.flags.showDuration && inlineSpace}
+          {this.state.flags.showDatetime && (
+            <Mireco.Datetime
+              value={this.state.formValue.datetime}
+              onChange={(newValue) => {
+                this.updateFormValue('datetime', newValue)
+              }}
+              disabled={this.state.flags.disabled}
+              block={this.state.flags.blockMode}
+            />
+          )}
+          {this.state.flags.showDatetime && inlineSpace}
+          {this.state.flags.showDatetimeRange && (
+            <Mireco.DatetimeRange
+              value={this.state.formValue.datetime_range}
+              onChange={(newValue) => {
+                this.updateFormValue('datetime_range', newValue)
+              }}
+              disabled={this.state.flags.disabled}
+              block={this.state.flags.blockMode}
+            />
+          )}
+          {this.state.flags.showDatetimeRange && inlineSpace}
+          <Mireco.Button
+            type="submit"
+            block={this.state.flags.blockMode}
+            disabled={this.state.flags.disabled}
+          >
+            Submit Results
+          </Mireco.Button>
+        </form>
+        {!this.state.flags.disableCss && (
+          <link rel="stylesheet" href="node_modules/mireco/dist/mireco-components.css" />
+        )}
+      </div>
+    )
+  }
+}
+
+const mount = document.querySelectorAll('div.mireco-demo-mount')
+if (mount.length > 0) {
+  ReactDOM.render(
+    <Demo />,
+    mount[0]
+  )
+}
