25f7a2102fac1915e8857726f95fea70b38bf97b
Michael Boreham <bor3ham@gmail.com>
2019-05-21 11:24:25 +1000

    Add todo note

--- src/inputs/basic/date.js
+++ src/inputs/basic/date.js
@@ -82,6 +82,7 @@ export default class MirecoDate extends React.Component {
       if (typeof valid !== 'undefined') {
         return
       }
+      // todo: work with values in UTC not localtime
       let parsed = parse(trimmed, format, new Date())
       if (isValid(parsed)) {
         // console.log(textValue, 'valid format:', format, this.format(this.props, +parsed))
