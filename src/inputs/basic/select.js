4d73c0cf79b341dfa6cdcc7ba40222dda4dfbb83
Michael Boreham <bor3ham@gmail.com>
2020-08-06 12:28:33 +1000

    Hookify select component

--- src/inputs/basic/select.js
+++ src/inputs/basic/select.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
@@ -12,6 +12,14 @@ const ENTER = 13
 
 const valueType = PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool])
 
+function usePrevious(value) {
+  const ref = useRef()
+  useEffect(() => {
+    ref.current = value
+  }, [value])
+  return ref.current
+}
+
 function validChoice(value, props) {
   return (
     typeof props.value === 'string'
@@ -20,74 +28,27 @@ function validChoice(value, props) {
   )
 }
 
-export default class Select extends React.PureComponent {
-  static propTypes = {
-    value: valueType,
-    nullable: PropTypes.bool,
-    options: PropTypes.arrayOf(PropTypes.shape({
-      value: valueType,
-      label: PropTypes.string,
-    })).isRequired,
-    placeholder: PropTypes.string,
-    block: PropTypes.bool,
-    disabled: PropTypes.bool,
-    onChange: PropTypes.func,
-    style: PropTypes.object,
+function Select(props) {
+  const containerRef = useRef(null)
+  const textRef = useRef(null)
+  const dropdownRef = useRef(null)
+
+  let initialText = ''
+  if (validChoice(props.value, props)) {
+    const initialChoice = props.options.find(option => {
+      return option.value === props.value
+    })
+    initialText = initialChoice ? initialChoice.label : `${props.value}`
   }
-  static defaultProps = {
-    nullable: true,
-    options: [],
-  }
-  constructor(props) {
-    super(props)
-    this.containerRef = React.createRef()
-    this.textRef = React.createRef()
-    this.dropdownRef = React.createRef()
-    let initialText = ''
-    if (validChoice(this.props.value, this.props)) {
-      const current = this.props.options.find(option => {
-        return option.value === this.props.value
-      })
-      initialText = current ? current.label : `${this.props.value}`
-    }
-    this.state = {
-      ...this.state,
-      textValue: initialText,
-      dropdownOpen: false,
-    }
-  }
-  componentDidUpdate = (prevProps, prevState) => {
-    if (prevProps.value !== this.props.value ) {
-      if (this.props.value === null) {
-        this.setState({textValue: ''})
-      }
-      else if (validChoice(this.props.value, this.props)) {
-        const filtered = this.getFilteredOptions()
-        let current = filtered.find(option => {
-          return (option.value === this.props.value)
-        })
-        if (!current) {
-          current = this.props.options.find(option => {
-            return (option.value === this.props.value)
-          })
-          if (current) {
-            this.setState({textValue: current.label})
-          }
-          else {
-            this.setState({textValue: `${this.props.value}`})
-          }
-        }
-      }
-    }
-    if (this.props.disabled && !prevProps.disabled) {
-      this.onBlur()
-    }
-  }
-  getFilteredOptions = () => {
-    const terms = this.state.textValue.split(' ').map(term => {
+
+  const [text, setText] = useState(initialText)
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+
+  const getFilteredOptions = () => {
+    const terms = text.split(' ').map(term => {
       return term.trim().toLowerCase()
     })
-    return this.props.options.filter(option => {
+    return props.options.filter(option => {
       const searchable = `${option.label}${option.value}`.toLowerCase()
       let match = false
       terms.map(term => {
@@ -98,49 +59,68 @@ export default class Select extends React.PureComponent {
       return match
     })
   }
-  handleContainerBlur = (event) => {
+  const onBlur = () => {
+    if (validChoice(props.value, props)) {
+      const selectedOption = props.options.find(option => {
+        return option.value === props.value
+      })
+      const formatted = selectedOption ? selectedOption.label : `${props.value}`
+      setText(formatted)
+      setDropdownOpen(false)
+      if (typeof props.onChange === 'function') {
+        props.onChange(props.value, true)
+      }
+    }
+    else {
+      setText('')
+      setDropdownOpen(false)
+      if (typeof props.onChange === 'function') {
+        props.onChange(null, true)
+      }
+    }
+  }
+  const handleContainerBlur = (event) => {
     if (
-      this.containerRef.current
-      && this.containerRef.current.divRef.current
+      containerRef.current
+      && containerRef.current.divRef.current
       && (
-        this.containerRef.current.divRef.current.contains(event.relatedTarget)
-        || this.containerRef.current.divRef.current === event.relatedTarget
+        containerRef.current.divRef.current.contains(event.relatedTarget)
+        || containerRef.current.divRef.current === event.relatedTarget
       )
     ) {
       // ignore internal blur
       return
     }
-    this.onBlur()
+    onBlur()
   }
-  handleTextFocus = (event) => {
-    this.setState({
-      dropdownOpen: true,
-    })
+  const handleTextFocus = (event) => {
+    setDropdownOpen(true)
   }
-  handleTextKeyDown = (event) => {
+  const handleTextKeyDown = (event) => {
     if (event.which === ENTER ) {
-      if (this.state.dropdownOpen) {
-        const current = this.props.options.find(option => {
-          return option.value === this.props.value
+      if (dropdownOpen) {
+        const current = props.options.find(option => {
+          return option.value === props.value
         })
-        this.setState({dropdownOpen: false, textValue: current ? current.label : ''})
+        setDropdownOpen(false)
+        setText(current ? current.label : '')
         event.preventDefault()
       }
       return
     }
-    if (!this.state.dropdownOpen) {
-      this.setState({dropdownOpen: true})
+    if (!dropdownOpen) {
+      setDropdownOpen(true)
     }
     if (event) {
       if (event.which === ARROW_DOWN || event.which === ARROW_UP) {
         event.preventDefault()
-        if (typeof this.props.onChange !== 'function') {
+        if (typeof props.onChange !== 'function') {
           return
         }
         let currentIndex = -1
-        const filtered = this.getFilteredOptions()
+        const filtered = getFilteredOptions()
         filtered.map((option, index) => {
-          if (option.value === this.props.value) {
+          if (option.value === props.value) {
             currentIndex = index
           }
         })
@@ -158,131 +138,162 @@ export default class Select extends React.PureComponent {
           }
         }
         if (filtered[nextIndex]) {
-          this.props.onChange(filtered[nextIndex].value)
+          props.onChange(filtered[nextIndex].value)
         }
         else {
-          this.props.onChange(null)
+          props.onChange(null)
         }
       }
     }
   }
-  handleTextChange = (newValue) => {
-    this.setState({textValue: newValue}, () => {
-      if (typeof this.props.onChange !== 'function') {
-        return
-      }
-      let cleaned = newValue.trim().toLowerCase()
-      if (cleaned.length <= 0) {
-        this.props.onChange(null, false)
+  const handleTextChange = (newValue) => {
+    setText(newValue)
+    if (typeof props.onChange !== 'function') {
+      return
+    }
+    let cleaned = newValue.trim().toLowerCase()
+    if (cleaned.length <= 0) {
+      props.onChange(null, false)
+    }
+    else {
+      let valueMatch = null
+      props.options.map(option => {
+        const optionValue = `${option.value}`.trim().toLowerCase()
+        if (valueMatch === null && optionValue === cleaned) {
+          valueMatch = option.value
+        }
+      })
+      if (valueMatch !== null) {
+        props.onChange(valueMatch, false)
       }
       else {
-        let valueMatch = null
-        this.props.options.map(option => {
-          const optionValue = `${option.value}`.trim().toLowerCase()
-          if (valueMatch === null && optionValue === cleaned) {
-            valueMatch = option.value
+        let labelMatch = null
+        props.options.map(option => {
+          const optionLabel = `${option.label}`.trim().toLowerCase()
+          if (labelMatch === null && optionLabel === cleaned) {
+            labelMatch = option.value
           }
         })
-        if (valueMatch !== null) {
-          this.props.onChange(valueMatch, false)
+        if (labelMatch !== null) {
+          props.onChange(labelMatch, false)
         }
         else {
-          let labelMatch = null
-          this.props.options.map(option => {
-            const optionLabel = `${option.label}`.trim().toLowerCase()
-            if (labelMatch === null && optionLabel === cleaned) {
-              labelMatch = option.value
-            }
+          const filtered = getFilteredOptions()
+          const current = filtered.find(option => {
+            return option.value === props.value
           })
-          if (labelMatch !== null) {
-            this.props.onChange(labelMatch, false)
-          }
-          else {
-            const filtered = this.getFilteredOptions()
-            const current = filtered.find(option => {
-              return option.value === this.props.value
-            })
-            this.props.onChange(current ? current.value : undefined, false)
-          }
+          props.onChange(current ? current.value : undefined, false)
         }
       }
-    })
+    }
   }
-  handleDropdownSelect = (value) => {
-    const selected = this.props.options.find(option => {
+  const handleDropdownSelect = (value) => {
+    const selected = props.options.find(option => {
       return option.value === value
     })
     if (!selected) {
       console.warn('Could not find selected value in options', value)
       return
     }
-    if (typeof this.props.onChange === 'function') {
-      this.props.onChange(value, true)
+    if (typeof props.onChange === 'function') {
+      props.onChange(value, true)
     }
-    this.textRef.current && this.textRef.current.focus()
-    this.setState({
-      dropdownOpen: false,
-      textValue: selected.label,
-    })
+    textRef.current && textRef.current.focus()
+    setDropdownOpen(false)
+    setText(selected.label)
   }
-  onBlur = () => {
-    if (validChoice(this.props.value, this.props)) {
-      const selectedOption = this.props.options.find(option => {
-        return option.value === this.props.value
-      })
-      let formatted = selectedOption ? selectedOption.label : `${this.props.value}`
-      this.setState({
-        textValue: formatted,
-        dropdownOpen: false,
-      }, () => {
-        if (typeof this.props.onChange === 'function') {
-          this.props.onChange(this.props.value, true)
-        }
-      })
+
+  const setTextFromPropValue = () => {
+    const current = props.options.find(option => {
+      return (option.value === props.value)
+    })
+    if (current) {
+      setText(current.label)
     }
     else {
-      this.setState({
-        textValue: '',
-        dropdownOpen: false,
-      }, () => {
-        if (typeof this.props.onChange === 'function') {
-          this.props.onChange(null, true)
-        }
-      })
+      setText(`${props.value}`)
     }
   }
-  render() {
-    const filtered = this.getFilteredOptions()
-    return (
-      <BlockDiv
-        ref={this.containerRef}
-        block={this.props.block}
-        className={classNames('MIRECO-select', {
-          'has-value': !!this.props.value,
-        })}
-        tabIndex={-1}
-        onBlur={this.handleContainerBlur}
-      >
-        <Text
-          ref={this.textRef}
-          placeholder={this.props.placeholder}
-          value={this.state.textValue}
-          onFocus={this.handleTextFocus}
-          onKeyDown={this.handleTextKeyDown}
-          onChange={this.handleTextChange}
-          disabled={this.props.disabled}
-          block={this.props.block}
-          style={this.props.style}
+
+  // on component received new props
+  const prevProps = usePrevious(props)
+  useEffect(() => {
+    if (!prevProps) {
+      return
+    }
+    if (prevProps.value !== props.value ) {
+      if (props.value === null) {
+        setText('')
+      }
+      else if (validChoice(props.value, props)) {
+        if (!dropdownOpen) {
+          setTextFromPropValue()
+        }
+        else {
+          const filtered = getFilteredOptions()
+          const current = props.options.find(option => {
+            return (option.value === props.value)
+          })
+          if (!current) {
+            setTextFromPropValue()
+          }
+        }
+      }
+    }
+    if (props.disabled && !prevProps.disabled) {
+      onBlur()
+    }
+  })
+
+  const filtered = getFilteredOptions()
+  return (
+    <BlockDiv
+      ref={containerRef}
+      block={props.block}
+      className={classNames('MIRECO-select', {
+        'has-value': !!props.value,
+      })}
+      tabIndex={-1}
+      onBlur={handleContainerBlur}
+    >
+      <Text
+        ref={textRef}
+        placeholder={props.placeholder}
+        value={text}
+        onFocus={handleTextFocus}
+        onKeyDown={handleTextKeyDown}
+        onChange={handleTextChange}
+        disabled={props.disabled}
+        block={props.block}
+        style={props.style}
+      />
+      {dropdownOpen && (
+        <Dropdown
+          ref={dropdownRef}
+          options={filtered}
+          value={props.value}
+          onSelect={handleDropdownSelect}
         />
-        {this.state.dropdownOpen && (
-          <Dropdown
-            ref={this.dropdownRef}
-            options={filtered}
-            value={this.props.value}
-            onSelect={this.handleDropdownSelect}
-          />
-        )}
-      </BlockDiv>
-    )
-  }
+      )}
+    </BlockDiv>
+  )
 }
+Select.propTypes = {
+  value: valueType,
+  nullable: PropTypes.bool,
+  options: PropTypes.arrayOf(PropTypes.shape({
+    value: valueType,
+    label: PropTypes.string,
+  })).isRequired,
+  placeholder: PropTypes.string,
+  block: PropTypes.bool,
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  style: PropTypes.object,
+}
+Select.defaultProps = {
+  nullable: true,
+  options: [],
+}
+
+export default Select
