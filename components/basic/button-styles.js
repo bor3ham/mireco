f04533df0c26f157b41f58a89bed6adf93bc2517
Michael Boreham <bor3ham@gmail.com>
2020-10-06 15:14:48 +1100

    Add demo of button styles

---
+++ components/basic/button-styles.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Button } from 'mireco'
+
+function DemoButtonStyles(props) {
+  return (
+    <>
+      <p>Standard:</p>
+      <p>
+        <Button>Primary</Button>
+        {' '}
+        <Button className="secondary">Secondary</Button>
+        {' '}
+        <Button className="content">Content</Button>
+        {' '}
+        <Button disabled>Disabled</Button>
+      </p>
+      <p>Outlined:</p>
+      <p>
+        <Button className="outline">Primary</Button>
+        {' '}
+        <Button className="secondary outline">Secondary</Button>
+        {' '}
+        <Button className="content outline">Content</Button>
+        {' '}
+        <Button className="outline" disabled>Disabled</Button>
+      </p>
+      <p>Links with button classes:</p>
+      <p>
+        <a href="#" className="MIRECO-button">Primary</a>
+        {' '}
+        <a href="#" className="MIRECO-button secondary">Secondary</a>
+        {' '}
+        <a href="#" className="MIRECO-button content">Content</a>
+        {' '}
+        <a className="MIRECO-button disabled">Disabled</a>
+      </p>
+    </>
+  )
+}
+
+const mount = document.querySelector('div.demo-mount-button-styles')
+if (mount) {
+  ReactDOM.render(<DemoButtonStyles />, mount)
+}
---
+++ components/basic/button.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import { Button } from 'mireco'
+
+function DemoButton(props) {
+  const [numPresses, setNumPresses] = useState(0)
+  const handleClick = () => {
+    setNumPresses(numPresses + 1)
+  }
+  return (
+    <>
+      <p>Button pressed: {numPresses} time{numPresses === 1 ? '' : 's'}</p>
+      <Button onClick={handleClick}>Press Me</Button>
+    </>
+  )
+}
+
+const mount = document.querySelector('div.demo-mount-button')
+if (mount) {
+  ReactDOM.render(<DemoButton />, mount)
+}
