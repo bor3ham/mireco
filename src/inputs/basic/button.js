61a33c347f0ba8681852684a89a4a9f57d245681
Michael Boreham <bor3ham@gmail.com>
2020-10-06 15:30:21 +1100

    Update button to function component

--- src/inputs/basic/button.js
+++ src/inputs/basic/button.js
@@ -2,48 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
-export default class Button extends React.PureComponent {
-  static propTypes = {
-    onClick: PropTypes.func,
-    type: PropTypes.oneOf(['button', 'submit']),
-    name: PropTypes.string,
-    value: PropTypes.string,
-    disabled: PropTypes.bool,
-    autoFocus: PropTypes.bool,
-    tabIndex: PropTypes.number,
+function Button(props) {
+  return (
+    <button
+      onClick={props.onClick}
+      type={props.type}
+      name={props.name}
+      value={props.value}
+      disabled={props.disabled}
+      autoFocus={props.autoFocus}
+      tabIndex={props.tabIndex}
 
-    children: PropTypes.node,
-    block: PropTypes.bool,
-    className: PropTypes.string,
-    style: PropTypes.object,
-  }
-  static defaultProps = {
-    type: 'button',
-    tabIndex: 0,
-    disabled: false,
-  }
-  render() {
-    return (
-      <button
-        onClick={this.props.onClick}
-        type={this.props.type}
-        name={this.props.name}
-        value={this.props.value}
-        disabled={this.props.disabled}
-        autoFocus={this.props.autoFocus}
-        tabIndex={this.props.tabIndex}
-
-        className={classNames(
-          'MIRECO-button',
-          {
-            block: this.props.block,
-          },
-          this.props.className,
-        )}
-        style={this.props.style}
-      >
-        {this.props.children}
-      </button>
-    )
-  }
+      className={classNames(
+        'MIRECO-button',
+        {
+          block: props.block,
+        },
+        props.className,
+      )}
+      style={props.style}
+    >
+      {props.children}
+    </button>
+  )
 }
+Button.propTypes = {
+  onClick: PropTypes.func,
+  type: PropTypes.oneOf(['button', 'submit']),
+  name: PropTypes.string,
+  value: PropTypes.string,
+  disabled: PropTypes.bool,
+  autoFocus: PropTypes.bool,
+  tabIndex: PropTypes.number,
+
+  children: PropTypes.node,
+  block: PropTypes.bool,
+  className: PropTypes.string,
+  style: PropTypes.object,
+}
+Button.defaultProps = {
+  type: 'button',
+  tabIndex: 0,
+  disabled: false,
+}
+
+export default Button
