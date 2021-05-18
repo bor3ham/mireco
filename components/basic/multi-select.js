6c9d858804c30497447870135d12e8e94b258498
Michael Boreham <bor3ham@gmail.com>
2021-05-18 13:42:21 +1000

    Add basic multiselect funcitonality

---
+++ components/basic/multi-select.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import { MultiSelect } from 'mireco/inputs'
+
+const OPTIONS = [
+  {
+    value: 'bike',
+    label: 'Bicycle',
+  },
+  {
+    value: 'cyclone',
+    label: 'Cyclone',
+  },
+  {
+    value: 'wash_cycle',
+    label: 'Wash Cycle',
+  },
+  {
+    value: 'binoculars',
+    label: 'Binoculars',
+  },
+]
+
+function DemoMultiSelect(props) {
+  const [value, setValue] = useState([])
+  const handleValueChange = (newValue, wasBlur) => {
+    setValue(newValue)
+  }
+  return (
+    <>
+      <p>Field value: {JSON.stringify(value) || 'undefined'}</p>
+      <MultiSelect
+        // block
+        placeholder="Select values"
+        value={value}
+        options={OPTIONS}
+        onChange={handleValueChange}
+      />
+    </>
+  )
+}
+
+const mount = document.querySelectorAll('div.demo-mount-multi-select')
+if (mount.length) {
+  ReactDOM.render(<DemoMultiSelect />, mount[0])
+}
