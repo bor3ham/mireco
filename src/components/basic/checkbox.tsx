473dc9e2cce95da8c8ff9fa16b87a221e0e29770
Michael Boreham <bor3ham@gmail.com>
2023-05-26 15:27:38 +1000

    Start input refresh

---
+++ src/components/basic/checkbox.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useCallback } from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { Checkbox } from 'mireco'
+
+const DemoCheckbox = () => {
+  const [value, setValue] = useState(false)
+  const handleChange = useCallback((newValue: boolean) => {
+    setValue(newValue)
+  }, [])
+  return (
+    <>
+      <p>Checked: {value ? 'Yes' : 'No'}</p>
+      <Checkbox value={value} onChange={handleChange}>Hi there</Checkbox>
+    </>
+  )
+}
+
+const container = document.querySelector('div.demo-mount-checkbox')
+if (container) {
+  const root = ReactDOM.createRoot(container)
+  root.render(<DemoCheckbox />)
+}
